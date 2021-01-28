080c8a5e97eabe2ec610f87b719db7034058d3eb
tonip9653 <taeyoung.park@uconn.edu>
2021-01-27 22:27:23 -0500

    we made basic navigation bar!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/dashboard', pathMatch:"full"},
+  {path:'', component:NavBarComponent, children:[
+    {path:'dashboard', component:DashboardComponent}
+  ]},
+  {path:'**', redirectTo:'/dashboard'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
